0bdad0d7603475cb0de04c4c18619248e488c589
Sam Evans <evansst@icloud.com>
2020-07-28 17:55:11 -0600

    Adds titlecasing for seed recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,35 +8,36 @@
 
 # RECIPE SEEDS
 # Add as many terms as you like to this array, and it should still work.  Just add a word without any commas
-# Recipe.destroy_all
 
-# search_array = %w[bread flour sugar milk eggs beef chicken pork turkey fish potatoes onions garlic tomatoes shallots
-#                   apple orange basil spinach avocado rice butter cheese onion corn lamb shrimp bacon coconut beets
-#                   pasta sphaghetti ravioli linguini penne steak burger sandwhich noodles chicken beef broccoli soy
-#                   breakfast lunch dinner]
+Recipe.destroy_all
 
-# counter = 1
+search_array = %w[bread flour sugar milk eggs beef chicken pork turkey fish potatoes onions garlic tomatoes shallots
+                  apple orange basil spinach avocado rice butter cheese onion corn lamb shrimp bacon coconut beets
+                  pasta sphaghetti ravioli linguini penne steak burger sandwhich noodles chicken beef broccoli soy
+                  breakfast lunch dinner]
 
-# search_array.each do |search_term|
-#   base_url1 = "https://api.edamam.com/search?q=#{search_term}&app_id=0bc71582&app_key=28c7d1686792de154523a2e314dc54c3&from=0&to=100"
-#   base_url2 = "https://api.edamam.com/search?q=#{search_term}s&app_id=0ea652a3&app_key=01e1c856909da6cd7fed1505d85975a9&from=0&to=100"
+counter = 1
 
-#   data1 = RestClient.get(base_url1)
-#   data2 = RestClient.get(base_url2)
+search_array.each do |search_term|
+  base_url1 = "https://api.edamam.com/search?q=#{search_term}&app_id=0bc71582&app_key=28c7d1686792de154523a2e314dc54c3&from=0&to=100"
+  base_url2 = "https://api.edamam.com/search?q=#{search_term}s&app_id=0ea652a3&app_key=01e1c856909da6cd7fed1505d85975a9&from=0&to=100"
 
-#   recipe_data = JSON.parse(data1)['hits'] + JSON.parse(data2)['hits']
+  data1 = RestClient.get(base_url1)
+  data2 = RestClient.get(base_url2)
 
-#   recipe_data.each do |recipe|
-#     Recipe.find_or_create_by(name: recipe['recipe']['label']) do |our_recipe|
-#       our_recipe.url = recipe['recipe']['url']
-#       our_recipe.image = recipe['recipe']['image']
-#       our_recipe.ingredients = recipe['recipe']['ingredientLines']
-#     end
-#   end
-#   p "search term #{counter} added to seeds! #{Recipe.all.count} recipes in the DB"
-#   counter += 1
-#   sleep(13) unless search_term == search_array.last
-# end
+  recipe_data = JSON.parse(data1)['hits'] + JSON.parse(data2)['hits']
+
+  recipe_data.each do |recipe|
+    Recipe.find_or_create_by(name: recipe['recipe']['label'].titlecase) do |our_recipe|
+      our_recipe.url = recipe['recipe']['url']
+      our_recipe.image = recipe['recipe']['image']
+      our_recipe.ingredients = recipe['recipe']['ingredientLines']
+    end
+  end
+  p "search term #{counter} added to seeds! #{Recipe.all.count} recipes in the DB"
+  counter += 1
+  sleep(13) unless search_term == search_array.last
+end
 
 # USER SEEDS
 
