fd7d8e92264c0c43551aa2df8ca687921ba07e7f
Sam Evans <evansst@icloud.com>
2020-07-29 14:38:30 -0600

    Adds update method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,30 +10,30 @@ class UsersController < ApplicationController
 
     render json: @user, include: %i[recipes shopping_list]
   end
-
+  
   def create
     @user = User.create(
       name: params[:name],
       username: params[:username]
     )
-
+    
     redirect_to("http://localhost:3001/user.html?user_id=#{@user.id}")
   end
-
+  
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
+    
     render json: { message: "User #{@user.username} has been deleted!" }
   end
-
+  
   def update
     @user = User.find(params[:id])
-    @user.update(
-      name: params[:name],
-      username: params[:username]
-    )
-
-    redirect_to("http://localhost:3001/user.html?user_id=#{@user.id}")
+    
+    @user.shopping_list.ingredients << params[:shopping_list]
+    @user.shopping_list.save!
+    
+    # redirect_to("http://localhost:3001/user.html?user_id=#{@user.id}")
+    render json: @user, include: %i[recipes shopping_list]
   end
 end
