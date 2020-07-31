89051102a8583d8ed0e41b6f880f7afa8c55cb00
Sam Evans <evansst@icloud.com>
2020-07-30 22:12:28 -0600

    Adds logic to add or remove items from the shopping list with a put request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,33 +10,33 @@ class UsersController < ApplicationController
 
     render json: @user, include: %i[recipes shopping_list]
   end
-  
+
   def create
     @user = User.create(
       name: params[:name],
       username: params[:username]
     )
-    
+
     redirect_to("http://localhost:3001/user.html?user_id=#{@user.id}")
   end
-  
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    
+
     render json: { message: "User #{@user.username} has been deleted!" }
   end
-  
+
   def update
     @user = User.find(params[:id])
 
-    if (params[:delete])
+    if params[:delete]
       @user.shopping_list.ingredients.delete(params[:shopping_list])
     else
       @user.shopping_list.ingredients << params[:shopping_list]
     end
+
     @user.shopping_list.save!
-    # redirect_to("http://localhost:3001/user.html?user_id=#{@user.id}")
     render json: @user, include: %i[recipes shopping_list]
   end
 end
